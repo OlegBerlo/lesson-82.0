d59311ceb90cd56522bac64ab01602430039be50
Oleg <oleg.berlo@ukr.net>
2023-05-20 08:27:49 +0300

    React Fragment

--- react-online-28-02/shop-project/src/index.tsx
+++ react-online-28-02/shop-project/src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// React Element
+// const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+
+//JSX
+const app = <h1 id="text">Hello App</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
